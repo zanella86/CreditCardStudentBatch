df8c5b7f849f2eb053082bc4e22b52d538bf9359
Thiago <thiago_zanella@yahoo.com.br>
2023-03-10 00:03:38 -0300

    #1 Elaboração da estrutura da Carga em Lote - Student

---
+++ src/main/java/com/fiap/onescjr/creditcardstudentbatch/integrator/StudentIn.java
@@ -0,0 +1,10 @@
+package com.fiap.onescjr.creditcardstudentbatch.integrator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class StudentIn {
+    private String student;
+}
