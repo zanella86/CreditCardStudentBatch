1075cd4feabec44f5d44ff566f4dfb64b01673b2
Thiago <thiago_zanella@yahoo.com.br>
2023-03-09 23:59:32 -0300

    #2 Adicionados os Spring Boot e Spring Batch

---
+++ src/main/java/com/fiap/onescjr/creditcardstudentbatch/CreditcardstudentbatchApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.onescjr.creditcardstudentbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardstudentbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardstudentbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/onescjr/creditcardstudentbatch/CreditcardstudentbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.onescjr.creditcardstudentbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardstudentbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
